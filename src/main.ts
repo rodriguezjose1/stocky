d90c50ad7725db3b819d0648e98261efa387e303
Jose Rodriguez <jrodriguez@denwaip.com>
2024-07-22 23:57:17 -0300

    add env port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   app.setGlobalPrefix('/api');
-  await app.listen(3000);
+  await app.listen(process.env.NODE_ENV || 8080);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
