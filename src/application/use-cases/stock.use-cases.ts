5e71bd09b84c028daf2675a90e1211a34f7c04a6
Jose Rodriguez <jrodriguez@denwaip.com>
2025-01-08 00:23:41 -0300

    add console with stringigy in create stock

--- src/application/use-cases/stock.use-cases.ts
+++ src/application/use-cases/stock.use-cases.ts
@@ -159,6 +159,7 @@ export class StockUseCases {
       return { stock, isCreated };
     } catch (error) {
       // await session.abortTransaction();
+      console.log('create-stock-error', JSON.stringify(error));
       throw error;
     }
   }
