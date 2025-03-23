df8d566e2e9997a603b2269706b77b23373afdb8
Jose Rodriguez <jrodriguez@denwaip.com>
2025-03-23 18:32:23 -0300

    fix set sizes when size type has been changed

--- src/application/use-cases/product.use-cases.ts
+++ src/application/use-cases/product.use-cases.ts
@@ -92,6 +92,7 @@ export class ProductUseCases {
       });
     }
 
+    let isUpdatedCategories = false;
     if (product.categories && !this.areArraysEqual(product.categories, productDB.categories)) {
       const categories = await this.categoryUseCases.getCategoriesBy({ _id: { $in: product.categories.map((id) => new Types.ObjectId(id)) } });
 
@@ -104,6 +105,13 @@ export class ProductUseCases {
       const categoryPaths = this.buildCategoryPaths(categories);
       product.categoriesFilter = categoryPaths;
 
+      const productAttributeSubtypeSize = await this.productAttributesSubtypeUseCases.getProductAttributeSubtypeById(product.sizeType);
+      const sizes = await this.productAttributesUseCases.getProductAttributes('size', productAttributeSubtypeSize.value);
+      product.sizes = sizes.map((size) => size.label || size.value);
+      isUpdatedCategories = true;
+    }
+
+    if (product.sizeType && !isUpdatedCategories) {
       const productAttributeSubtypeSize = await this.productAttributesSubtypeUseCases.getProductAttributeSubtypeById(product.sizeType);
       const sizes = await this.productAttributesUseCases.getProductAttributes('size', productAttributeSubtypeSize.value);
       product.sizes = sizes.map((size) => size.label || size.value);
